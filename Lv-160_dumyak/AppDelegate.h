a0f191f3800f8f06a6b9e8c8005021b9a0cad582
Admin <admin@Mac-Admin.local>
2015-08-29 13:39:41 +0300

    Initial Commit

---
+++ Lv-160_dumyak/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lv-160_dumyak
+//
+//  Created by Admin on 29.08.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lv-160_dumyak/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lv-160_dumyak
+//
+//  Created by Admin on 29.08.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
